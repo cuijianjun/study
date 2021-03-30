84a0671bf233b79a9bdc9e3a0f8a64797112c4fa
Frank <376247934@qq.com>
2021-03-30 22:32:42 +0800

    vuex init

---
+++ vue_com_test/src/kstore/kvuex.js
@@ -0,0 +1,12 @@
+// 1. 实现操作
+
+class Store {
+
+}
+
+function install (Vue) {
+
+}
+
+// 导出的对象就是vuex
+export default {store, install}
--- vue_com_test/src/main.js
+++ vue_com_test/src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 // import router from './router'
 import router from './krouter'
+import store from './kstore'
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   router, // 让vue在插件中，可以获取到vue的实例
   render: h => h(App),
 }).$mount('#app')
