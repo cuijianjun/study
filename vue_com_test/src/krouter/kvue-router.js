66c90c6f035e851cb9b14227e2452bdb249a7d6b
Frank <376247934@qq.com>
2021-03-30 17:36:00 +0800

    vue-router 阉割版完成

--- vue_com_test/src/krouter/kvue-router.js
+++ vue_com_test/src/krouter/kvue-router.js
@@ -7,7 +7,28 @@ let _Vue
 class VueRouter {
   constructor(options) {
     this.$options = options
+
+    // 缓存path和route的映射关系
+    this.routeMap = {}
+    this.$options.routes.forEach(
+      route => {
+        this.routeMap[route.path] = route
+      }
+    )
+    // 需要定义一个响应式的current属性
+    const initial = window.location.hash.slice(1) || '/'
+    _Vue.util.defineReactive(this, 'current', initial)
+
+    // 监控url变化
+    window.addEventListener('hashchange', this.onHashChange.bind(this))
   }
+
+  onHashChange() {
+    // 只要#后面的部分
+    this.current = window.location.hash.slice(1)
+  }
+  
+  
 }
 
 VueRouter.install = function (Vue) {
@@ -15,8 +36,42 @@ VueRouter.install = function (Vue) {
   _Vue = Vue
   Vue.mixin({
     beforeCreate() {
-      Vue.prototype.$router = this.$options.router
+      if (this.$options.router) {
+        Vue.prototype.$router = this.$options.router
+      }
     },
+  })
+  
+  // 定义两个全局的组件 router-link ，router-view
+  Vue.component('router-link', {
+    props: {
+      to: {
+        type: String,
+        require: true
+      }
+    },
+    render(h) {
+      // <a>XXX</a>
+      return h('a', {
+        attrs: {
+          href: '#' + this.to,
+        }
+      }, this.$slots.default)
+    }
+  })
+  Vue.component('router-view', {
+    render(h){
+      let component = null;
+      // 找到当前url对应的组件
+      // const route = this.$router.$options.routes.find(route => route.path === this.$router.current)
+      // if (route) {
+      //   component = route.component
+      // }
+      const {routeMap, current} = this.$router
+      component = routeMap[current]? routeMap[current].component: null
+      // 渲染传入组件
+      return h(component)
+    }
   })
   
 }
