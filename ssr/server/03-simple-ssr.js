38ba2e1809dee241d5af31723cb8ad67aebabfad
Frank <376247934@qq.com>
2021-04-05 13:10:40 +0800

    ssr

--- ssr/server/03-simple-ssr.js
+++ ssr/server/03-simple-ssr.js
@@ -1,32 +1,31 @@
 // node代码
-// express server
-const express = require('express')
+// express server 
+const express = require('express');
 const app = express()
 
 const Vue = require('vue')
-const {createRenderer} = require('vue-server-renderer')
+const {createRenderer} = require('vue-server-renderer');
 
 // 获取渲染器
-const renderer = createRenderer()
+const renderer = createRenderer();
 
-// 问题1：无法交互（客户端激活）
+// 问题1： 无法交互（客户端激活）
 // 问题2：同构开发
 app.get('*', async (req, res) => {
   req.url
   // 创建一个Vue实例
   const vm = new Vue({
-    data: {name:'村长真棒'},
+    data: {name: '你好'},
     template: '<div>{{name}}</div>'
   })
-  
+
   try {
-    // renderToString将Vue实例转换为html字符串
+    // renderToString 将vue实例转换为html字符串
     const html = await renderer.renderToString(vm)
     res.send(html)
   } catch (error) {
-    res.status(500).send('服务器渲染错误')
+    res.status(500).send('服务端渲染错误')
   }
-  
 })
 
 app.listen(3000)
