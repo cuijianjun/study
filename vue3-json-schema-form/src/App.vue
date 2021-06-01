d36bb9a2b49c1f141c465c66dc6b5708ae0529f4
Frank <376247934@qq.com>
2021-06-01 12:05:45 +0800

    defineComponent

--- vue3-json-schema-form/src/App.vue
+++ vue3-json-schema-form/src/App.vue
@@ -4,29 +4,29 @@
 </template>
 
 <script lang="ts">
-  import { defineComponent, PropType } from "vue";
-  import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
 
-  export default defineComponent({
-    name: "App",
-    components: {
-      HelloWorld,
-    },
-    data() {
-      return {
-        name: 'Frank'
-      }
-    },
-  });
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      name: 'Frank',
+    }
+  },
+})
 </script>
 
 <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- vue3-json-schema-form/src/components/HelloWorld.vue
+++ vue3-json-schema-form/src/components/HelloWorld.vue
@@ -1,45 +1,45 @@
 <template>
   <div class="hello">
-    {{age}}
+    {{ age }}
   </div>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from "vue";
-  const PropsType = {
-    msg: String,
-    age: {
-      type: Number,
-      require: true
-    }
-  } // 只读属性的更改方法  as const
+import { defineComponent } from 'vue'
+const PropsType = {
+  msg: String,
+  age: {
+    type: Number,
+    require: true,
+  },
+} // 只读属性的更改方法  as const
 
-  export default defineComponent({
-    name: "HelloWorld",
-    props: PropsType,
-    mounted() {
-      this.age
-    },
-  });
+export default defineComponent({
+  name: 'HelloWorld',
+  props: PropsType,
+  mounted() {
+    this.age
+  },
+})
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  h3 {
-    margin: 40px 0 0;
-  }
+h3 {
+  margin: 40px 0 0;
+}
 
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
+ul {
+  list-style-type: none;
+  padding: 0;
+}
 
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
 
-  a {
-    color: #42b983;
-  }
+a {
+  color: #42b983;
+}
 </style>
