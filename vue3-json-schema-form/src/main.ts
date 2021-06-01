c2d03c886a6bbe76df61f1585eeaa267665eadc5
Frank <376247934@qq.com>
2021-06-01 14:23:12 +0800

    h

--- vue3-json-schema-form/src/main.ts
+++ vue3-json-schema-form/src/main.ts
@@ -1,4 +1,25 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+import { createApp, defineComponent, h } from 'vue'
+// import App from './App.vue'
+
+const App = defineComponent({
+  render() {
+    return h(
+      'div',
+      {
+        id: 'app',
+      },
+      [
+        h('img', {
+          alt: 'Vue logo',
+          src: '',
+        }),
+        h(HelloWorld, {
+          msg: 'Welcome to ypur Vue.js + ts',
+        }),
+      ],
+    )
+  },
+})
 
 createApp(App).mount('#app')
