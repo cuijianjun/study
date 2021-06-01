f6bbad09bf10eee5b2b2d23921b86b92724bb668
Frank <376247934@qq.com>
2021-06-01 12:00:43 +0800

    defineComponent

--- vue3-json-schema-form/src/App.vue
+++ vue3-json-schema-form/src/App.vue
@@ -1,27 +1,32 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" :age="12" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+  import { defineComponent, PropType } from "vue";
+  import HelloWorld from "./components/HelloWorld.vue";
 
-export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-});
+  export default defineComponent({
+    name: "App",
+    components: {
+      HelloWorld,
+    },
+    data() {
+      return {
+        name: 'Frank'
+      }
+    },
+  });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
