13649615a0f79f69149d0cdb8f6327936141dcdc
Frank <376247934@qq.com>
2021-06-02 19:41:14 +0800

    nameRef

--- vue3-json-schema-form/src/App.vue
+++ vue3-json-schema-form/src/App.vue
@@ -4,29 +4,41 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+  import { defineComponent } from 'vue'
+  import HelloWorld from './components/HelloWorld.vue'
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-  data() {
-    return {
-      name: 'Frank',
+  export default defineComponent({
+    name: 'App',
+    components: {
+      HelloWorld,
+    },
+    // data() {
+    //   return {
+    //     name: 'Frank',
+    //   }
+    // },
+    setup(props, { slots, attrs, emit }) {
+      const nameRef = ref('jokcy')
+      setInterval(() => {
+        nameRef.value += "1"
+      }, 1000)
+
+      return {
+        name: nameRef
+      }
     }
-  },
-})
+
+
+  })
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- vue3-json-schema-form/src/main.ts
+++ vue3-json-schema-form/src/main.ts
@@ -1,25 +1,25 @@
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 import { createApp, defineComponent, h } from 'vue'
-// import App from './App.vue'
+import App from './App.vue'
 
-const App = defineComponent({
-  render() {
-    return h(
-      'div',
-      {
-        id: 'app',
-      },
-      [
-        h('img', {
-          alt: 'Vue logo',
-          src: '',
-        }),
-        h(HelloWorld, {
-          msg: 'Welcome to ypur Vue.js + ts',
-        }),
-      ],
-    )
-  },
-})
+// const App = defineComponent({
+//   render() {
+//     return h(
+//       'div',
+//       {
+//         id: 'app',
+//       },
+//       [
+//         h('img', {
+//           alt: 'Vue logo',
+//           src: '',
+//         }),
+//         h(HelloWorld, {
+//           msg: 'Welcome to ypur Vue.js + ts',
+//         }),
+//       ],
+//     )
+//   },
+// })
 
 createApp(App).mount('#app')
