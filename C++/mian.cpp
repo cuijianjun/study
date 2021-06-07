356020f8a1e200b3f14858a41d0cf46abd69bb31
Frank <376247934@qq.com>
2021-06-07 18:13:32 +0800

    杂项运算符

--- C++/mian.cpp
+++ C++/mian.cpp
@@ -2,10 +2,17 @@
 
 void main(void)
 {
-    bool bA = false, bB = true;
-    cout < < (bA == true || bA != true) < < endl;
-    cout < < (bB == true || bB != true) < < endl;
-    // 德*摩根率
-    cout < < (!(bA || bB) == (!bA && !bB)) < < endl;
-    cout < < (!(bA && bB) == (!bA || !bB)) < < endl;
+    char A = '1';
+    count << sizeof(A) << endl;
+    int C = A > B ? 1 : 0;
+    count << A<B? 1: 0 << endl;
+    count << D << endl;
+    int E = (A, B, C);
+    count << E << endl;
+    float F = float(E);
+    count << F << endl;
+    count << &F << endl;
+    float *p = &F;
+    count << P << endl;
+    count << *P << endl;
 }
