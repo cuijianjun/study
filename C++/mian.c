5b23da9861033706b19c1051b1cac650fdf8b135
Frank <376247934@qq.com>
2021-06-11 16:03:30 +0800

    变量

--- C++/mian.c
+++ C++/mian.c
@@ -12,5 +12,14 @@ int main() {
   printf("value: %d\n", value);
   value_init = value;
   printf("value");
+  printf("address of value: %#x\n", sizeof(value));
+  printf("address of value: %#x\n", &value);
+
+  // key words 标识符 identifier
+  // 1. a-zA-Z0-9
+  // 2. 数不能再第一位
+  // 3. Google code style
+  float a_float3 = 3.14f;
+  float a_float = 3.14f;
   return 0;
 }
