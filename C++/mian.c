3f424f41b19458114167d48bc130f3629bf02751
Frank <376247934@qq.com>
2021-06-11 11:05:32 +0800

    int

--- C++/mian.c
+++ C++/mian.c
@@ -1,13 +1,20 @@
 #include <stdio.h>;
 
 int main() {
-  int sum = 0;
+  short int short_int = 0;
   int i = 0;
-  while (i<10)
-  {
-    sum = sum + i;
-    i++;
-  }
-  printf("%d\n", sum);
-  return 0;
+  long int long_int = 0;
+  long long int longlong_int = 0;
+  // d = decimal
+  // x = hex
+  // 0 = oct
+  // hd%: short decimal
+  // %d: decimal
+  // %ld: long decimal
+  // %lld: long long decimal
+  // %d : decimal
+  // \n new line 
+  printf("short int : %d\n", sizeof(short int));
+  printf("int : %d\n", sizeof(int));
+  printf("long int : %d\n", sizeof(long int));
 }
