57e63b84c238c17d080b1d2a1f1945d8c7dd909d
Frank <376247934@qq.com>
2021-06-11 11:19:55 +0800

    char

--- C++/mian.c
+++ C++/mian.c
@@ -1,20 +1,22 @@
 #include <stdio.h>;
 
 int main() {
-  short int short_int = 0;
-  int i = 0;
-  long int long_int = 0;
-  long long int longlong_int = 0;
-  // d = decimal
-  // x = hex
-  // 0 = oct
-  // hd%: short decimal
-  // %d: decimal
-  // %ld: long decimal
-  // %lld: long long decimal
-  // %d : decimal
-  // \n new line 
-  printf("short int : %d\n", sizeof(short int));
-  printf("int : %d\n", sizeof(int));
-  printf("long int : %d\n", sizeof(long int));
+  // 字符集 ASCII
+  char a = 'a'; // 97
+  char char_1 = '1';
+  char char_0 = '0';
+
+  char i = 0; // \0, null
+  // 字面量 literal
+  // \n : new line
+  // \b: backspace
+  // \r:return
+  // \t: table
+
+  // Unicode CJK code point
+  // C95
+  wchar_t zhong = L"中";
+  wchar_t zhogn_hex = L'\u4E2D';
+  printf("中：%d\n", zhong);
+  printf("中： %d\n", zhogn_hex);
 }
