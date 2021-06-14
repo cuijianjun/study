10b22fe78e91b8290762a7507a240b7fc6cdd3b1
Frank <376247934@qq.com>
2021-06-14 10:16:32 +0800

    变量

--- C++/mian.c
+++ C++/mian.c
@@ -1,25 +1,11 @@
 #include <stdio.h>;
 
 int main() {
-  // <type> <name>
-  int value;
-  // <type> <name> = <initialized value>
-  int value_init = 3;
-
-  value = 4;
-  value_init = 5;
-
-  printf("value: %d\n", value);
-  value_init = value;
-  printf("value");
-  printf("address of value: %#x\n", sizeof(value));
-  printf("address of value: %#x\n", &value);
-
-  // key words 标识符 identifier
-  // 1. a-zA-Z0-9
-  // 2. 数不能再第一位
-  // 3. Google code style
-  float a_float3 = 3.14f;
-  float a_float = 3.14f;
+  // const <type> readonly variable
+  const int kRed = 0xFF0000;
+  printf("kRed: %d\n", kRed);
+  int *p_k_red = &kRed;
+  *p_k_red = 0;
+  printf("kRed: %\n", kRed);
   return 0;
 }
