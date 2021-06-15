533ea7dafd9b408e903d9e01e0827644dd0e9170
Frank <376247934@qq.com>
2021-06-15 20:33:20 +0800

    test

--- C++/mian.c
+++ C++/mian.c
@@ -1,22 +1,6 @@
 #include <stdio.h>;
 
 int main() {
-  // const <type> readonly variable
-  const int kRed = 0xFF0000;
-  printf("kRed: %d\n", kRed);
-  int *p_k_red = &kRed;
-  *p_k_red = 0;
-  printf("kRed: %\n", kRed);
-  // 字面量 literal
-  3;
-  3u;
-  3l;
-  3.f;
-  3.9;
-  'c';
-  "cs";
-  L'中'； L"中国";
-  // 硬编码 hard code
-  int background_color = kRed;
+  
   return 0;
 }
