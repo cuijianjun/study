8ba056ad3ede707157e0d97da40fb901ffe66bc4
Frank <376247934@qq.com>
2021-06-16 21:13:54 +0800

    运算符

--- C++/mian.c
+++ C++/mian.c
@@ -1,6 +1,29 @@
 #include <stdio.h>;
 
 int main() {
-  
+  int first = 0;
+  int second;
+  int third;
+  // =
+  third = second = first;
+
+  int left, right;
+  left = 2;
+  right = 3;
+
+  int sum;
+  sum = left + right;
+  int diff = left - right;
+  int product = left * right; // 6
+  int quotient = left / right;
+  int remainder = left % right;
+
+  int quitent_1 = 100 / 30; //
+
+  int i = 1;
+  int j = i++;
+  int k = ++i;
+  printf("3>3:%d\n", 3 > 2);
+  printf("3>2 || 3< 2%d\n", 3 > 3 || 3 < 2);
   return 0;
 }
