1bc616cbb31caeb4155a7942e821071f1ffe8995
Frank <376247934@qq.com>
2021-06-27 14:26:56 +0800

    函数

--- C++/mian.c
+++ C++/mian.c
@@ -24,3 +24,20 @@ int main(void) {
   printf("result of f: %f\n", return_g);
   return 0;
 }
+
+void EmptyParamList(void);
+
+/*
+  1. 函数名
+  2.函数的返回值类型，如果没写，默认为int
+  3. 函数参数列表，参数类型和参数顺序，参数形参顺序，参数形参名不重要
+*/
+
+int Add(int, int);
+
+int main2(void){
+  puts("");
+  EmptyParamList();
+  int result = Add(1, 2);
+  return 0;
+}
