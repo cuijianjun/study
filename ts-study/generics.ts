a7ab89fea35df77615260808acf9af2b39758842
Frank <376247934@qq.com>
2021-11-25 12:01:33 +0800

    generices

---
+++ ts-study/generics.ts
@@ -0,0 +1,12 @@
+function echo<T> (arg: T): T {
+  return arg
+}
+
+const result = echo(true)
+
+
+function swap<T, U> (tuple: [T, U]): [U, T] {
+  return [tuple[1], tuple[0]]
+}
+const result2 = swap(['string', 123])
+result2[1]
