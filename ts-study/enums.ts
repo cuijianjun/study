d8bfa7983b3b2529a0a7fe2a261ece935f4bb98d
Frank <376247934@qq.com>
2021-11-25 11:44:54 +0800

    enums

---
+++ ts-study/enums.ts
@@ -0,0 +1,15 @@
+enum Direction {
+  Up = "UP",
+  Down = "DOWN",
+  Left = "LEFT",
+  Right = "RIGHT",
+}
+
+console.log(Direction.Up);
+console.log(Direction[0]);
+
+const value = "UP"
+
+if (value === Direction.Up) {
+  console.log("go up");
+}
