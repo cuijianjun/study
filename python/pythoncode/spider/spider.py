0e6157866ed6b7d876db559081b4262444f37425
Frank <376247934@qq.com>
2022-07-06 08:06:51 +0800

    Spider-douyu

--- python/pythoncode/spider/spider.py
+++ python/pythoncode/spider/spider.py
@@ -1,20 +1,29 @@
 from urllib import request
 import ssl
 import gzip
+import re
 
 context = ssl._create_unverified_context()
 
 class Spider():
     url = 'https://www.douyu.com/g_LOL'
-
+    root_pattern = '<main class="layout-Main">[\s\S]*?</main>'
+    main_pattern = '<div class="DyListCover-info">[\s\S]*?</div>'
     def __fetch_content(self):
         r = request.urlopen(Spider.url, context=context)
         htmls = r.read()
         htmls = gzip.decompress(htmls).decode("utf-8")
         print(htmls)
+        return htmls
 
+    def __analysis(self, htmls):
+        root_html = re.findall(Spider.root_pattern, htmls)
+        main_html = re.findall(Spider.main_pattern, root_html[0])
+        a = 1
+       
     def go(self):
-        self.__fetch_content()
+        htmls = self.__fetch_content()
+        self.__analysis(htmls)
 
 sprider = Spider()
 sprider.go()
