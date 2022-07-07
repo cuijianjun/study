0e2060b83050ed785e8303aff312a2c6b404d571
Frank <376247934@qq.com>
2022-07-07 08:47:48 +0800

    Spider完成

--- python/pythoncode/spider/spider.py
+++ python/pythoncode/spider/spider.py
@@ -28,16 +28,36 @@ class Spider():
                 anchor = {'name': name, 'number': number}
                 anchors.append(anchor)
         return anchors
+    def __sort(self, anchors):
+        anchors = sorted(anchors, key=self.__sort_seed, reverse=True)
+        return anchors
+
+    def __sort_seed(self, anchor):
+        r = re.findall('(\d+\.?\d+)', anchor['number'])
+        print(r)
+        number = float(r[0])
+        if '万' in anchor['number']:
+            number *= 10000
+        return number
+
+    def __show(self, anchors):
+        for rank in range(0, len(anchors)):
+            print('rank' + str(rank + 1) + '  :  ' + anchors[rank]['name'] + '      ' + anchors[rank]['number'])
+
     def __refine(self, anchors):
         l = lambda anchor: {
             'name': anchor['name'][0].strip(),
             'number': anchor['number'][0]
             }
         return map(l, anchors)
+
     def go(self):
         htmls = self.__fetch_content()
         anchors = self.__analysis(htmls)
         anchors = list(self.__refine(anchors))
-        print(anchors)
+        anchors = self.__sort(anchors)
+        self.__show(anchors)
+
+
 sprider = Spider()
 sprider.go()
