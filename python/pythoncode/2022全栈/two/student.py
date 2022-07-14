84f4d00307d4bd09faf73b2f5bb42e6275cdbe8e
Frank <376247934@qq.com>
2022-07-14 19:36:36 +0800

    student-数据对象化

--- python/pythoncode/2022全栈/two/student.py
+++ python/pythoncode/2022全栈/two/student.py
@@ -4,6 +4,71 @@
 
 from re import A
 
+class StudentInfo(object):
+    def __init__(self, students):
+        self.students = students
+    def get_all_students(self):
+        for id_,value in self.students.items():
+            print('学号：{},姓名:{} ,年龄:{}, 性别:{}, 班级{}'.format(
+                id_,value['name'],value['age'],value['sex'],value['class_number']
+            ))
+        return student
+    
+    def get_user_by_id(self, id):
+        return self.students.get(id)
+    def add_student(self, **kwargs):
+        check = self.check_user_info(**kwargs)
+        if check != True:
+            print(check)
+            return
+        
+        id_ = max(self.students) + 1
+
+        self.students[id_] = {
+            'name': kwargs['name'],
+            'age': kwargs['age'],
+            'sex': kwargs['sex'],
+            'class_name': kwargs['class_name']
+        }
+    
+    def adds(self, new_students):
+        for student in new_students:
+            check = self.check_user_info(**student)
+            if check != True:
+                print(check, student.get('name'))
+                continue
+            self.__add(**student)
+    def __add(self, **student):
+        new_id = max(self.students) + 1
+        self.students[new_id] = student
+    
+    def delete_student(self, student_id):
+        if student_id not in self.students:
+            print('{}并不存在'.format(student_id))
+        else:
+            user_info = self.students.pop(student_id)
+            print('学号是{},{}同学的信息已经删除'.format(student_id, user_info))
+
+    def update_student(self, student_id, **kwargs):
+        if student_id in self.students:
+            print('并不存在这个学号:{}'.format(student_id))
+        check = self.check_user_info(**kwargs)
+        if check != True:
+            print(check)
+            return
+        self.students[student_id] = kwargs
+        print('同学信息更新完毕')
+    def check_user_info(self, **kwargs):
+        if 'name' not in kwargs:
+            return  '没有发现学生姓名'
+        if 'age' not in kwargs:
+            return '没有发现学生年龄'
+        if 'sex' not in kwargs:
+            return '没有发现学生性别'
+        if 'class_number' not in kwargs:
+            return  '没有发现学生学号'
+        return True
+
 
 student = {
     1: {
@@ -37,68 +102,7 @@ student = {
         'sex': 'gril'
     },
 }
-print(max(student))
 
-def check_user_info(**kwargs):
-    if 'name' not in kwargs:
-        return  '没有发现学生姓名'
-    if 'age' not in kwargs:
-        return '没有发现学生年龄'
-    if 'sex' not in kwargs:
-        return '没有发现学生性别'
-    if 'class_number' not in kwargs:
-        return  '没有发现学生学号'
-    return True
-
-
-def get_all_students():
-    for id_,value in student.items():
-        print('学号：{},姓名:{} ,年龄:{}, 性别:{}, 班级{}'.format(
-            id_,value['name'],value['age'],value['sex'],value['class_number']
-        ))
-    return student
-
-# get_all_students()
-
-def add_student(**kwargs):
-    check = check_user_info(**kwargs)
-    if check != True:
-        print(check)
-        return
-    
-    id_ = max(student) + 1
-
-    student[id_] = {
-        'name': kwargs['name'],
-        'age': kwargs['age'],
-        'sex': kwargs['sex'],
-        'class_name': kwargs['class_name']
-    }
-
-add_student(name='小白', age=19,class_number='A', sex='boy')
-
-def delete_student(student_id):
-    if student_id not in student:
-        print('{}并不存在'.format(student_id))
-    else:
-        user_info = student.pop(student_id)
-        print('学号是{},{}同学的信息已经删除'.format(student_id, user_info))
-
-def update_student(student_id, **kwargs):
-    if student_id in student:
-        print('并不存在这个学号:{}'.format(student_id))
-    check = check_user_info(**kwargs)
-    if check != True:
-        print(check)
-        return
-    student[student_id] = kwargs
-    print('同学信息更新完毕')
-
-update_student(1, name='dewei.zhang',age=33, class_number="A", sex='boy')
-
-def get_user_by_id(student_id):
-    return student.get(student_id)
-
-print(get_user_by_id(3))
-
-
+student_info = StudentInfo(student)
+user = student_info.get_user_by_id(1)
+print(user)
