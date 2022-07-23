50d5929624d2996ce3afea413e3845b4d393b198
Frank <376247934@qq.com>
2022-07-23 12:30:00 +0800

    内置视图

--- python/pythoncode/2022全栈/my_project/my_project/urls.py
+++ python/pythoncode/2022全栈/my_project/my_project/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import include
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,3 +30,12 @@ urlpatterns = [
     # path('hello/', hello_world, name='hello_world'),
     path('hello/', include('hello.urls'))
 ]
+
+handler500 = 'my_project.views.page_500'
+
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
