1c92e67a405ec71d820c9192f51f3acbe94b4775
Frank <376247934@qq.com>
2022-12-16 18:34:15 +0800

    eslint

--- vite/vite-vue3/src/main.js
+++ vite/vite-vue3/src/main.js
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App'
+import "./style.css";
+import App from "./App";
 
 import Worker from "./worker?workeer"
 const worker = new Worker();
-worker.onmessage = function(e) {
+worker.onmessage = function (e) {
     console.log(e);
 }
 createApp(App).mount('#app')
