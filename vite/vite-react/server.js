5d42847a59861ba42ed9d11db6e58757c4daf8a3
Frank <376247934@qq.com>
2022-12-18 05:50:52 +0800

    ssr

---
+++ vite/vite-react/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const { createServer } = require('vite');
+const fs = require("fs");
+const app = express();
+
+const {createServer: createViteServer} = require("vite");
+
+createViteServer(
+    {
+        server: {
+            middlewareMode: "html",
+        }
+    }
+).then((vite) => {
+    app.use(vite.middlewares);
+
+    app.get("*", async (req, res) => {
+        
+        const template = fs.readFileSync("index.html", "utf-8");
+        const render  = await vite.ssrLoadModule("/src/server-entry.jsx");
+        const html = await render(req.url);
+        const responseHtml = template.replace("<!-- APP_HTML -->", html);
+        res.set("content-type", "text/html").send(responseHtml);
+    })
+    app.listen(4000);
+})
