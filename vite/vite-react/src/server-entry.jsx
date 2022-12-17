ea70987ceaba525a5be58c35262834b98a5a3810
Frank <376247934@qq.com>
2022-12-18 06:33:40 +0800

    正式环境集成vite

--- vite/vite-react/src/server-entry.jsx
+++ vite/vite-react/src/server-entry.jsx
@@ -1,6 +1,6 @@
 import ReactDOMServer from "react-dom/server";
-import { StaticRouter } from "react-routeer-dom";
-import { App } from "./App";
+import { StaticRouter } from "react-router-dom";
+import App from "./App";
 
 export function render(url, context){
     return ReactDOMServer.renderToString(
