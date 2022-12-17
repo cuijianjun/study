9e77f7934389def38baff657ae0220bf5934dc76
Frank <376247934@qq.com>
2022-12-17 19:59:05 +0800

    glob import

--- vite/vite-vue3/src/main.js
+++ vite/vite-vue3/src/main.js
@@ -2,9 +2,18 @@ import { createApp } from 'vue'
 import "./style.css";
 import App from "./App";
 
-import Worker from "./worker?workeer"
-const worker = new Worker();
-worker.onmessage = function (e) {
-    console.log(e);
-}
+// import Worker from "./worker?workeer"
+// const worker = new Worker();
+// worker.onmessage = function (e) {
+//     console.log(e);
+// }
+
+const globModules = import.meta.glob('./glob')
+console.log(globModules);
+
+Object.entries(globModules).forEach(([k,v]) => {
+    console.log(k + ":");
+    v().then(m => console.log(m.default))
+})
+
 createApp(App).mount('#app')
