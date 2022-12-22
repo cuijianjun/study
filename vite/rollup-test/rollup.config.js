a0d6a182b78c499bb2ff1f3e8500449386999a08
Frank <376247934@qq.com>
2022-12-22 23:40:16 +0800

    rollup output

--- vite/rollup-test/rollup.config.js
+++ vite/rollup-test/rollup.config.js
@@ -1,6 +1,7 @@
 import json from "@rollup/plugin-json"
 import resolve from "@rollup/plugin-node-resolve"
 import commonjs from "@rollup/plugin-commonjs"
+import { terser } from "rollup-plugin-terser"
 export default [
     {
         input: "index.js",
@@ -9,7 +10,8 @@ export default [
         output: {
             file: "dist/index.umd.js",
             format: "umd",
-            name: "Index"
+            name: "Index",
+            plugins: [terser()],
         }
     },
     {
@@ -18,7 +20,8 @@ export default [
         output: {
             file: "dist/index.es.js",
             format: "es",
-            name: "Index"
+            name: "Index",
+            banner: "/** Hello this is Banner**/"
         }
     }
 ]
