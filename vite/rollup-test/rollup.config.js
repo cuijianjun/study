ba7e514d6960d48f6f29b40535ff4336dd3d0248
Frank <376247934@qq.com>
2022-12-24 15:09:01 +0800

    rollup finish

--- vite/rollup-test/rollup.config.js
+++ vite/rollup-test/rollup.config.js
@@ -2,13 +2,20 @@ import json from "@rollup/plugin-json"
 import resolve from "@rollup/plugin-node-resolve"
 import commonjs from "@rollup/plugin-commonjs"
 import { terser } from "rollup-plugin-terser"
+import ts from "@rollup/plugin-typescript"
+
 export default [
     {
         input: "index.js",
         external: ['react'],
-        plugins: [resolve(),commonjs(), json()],
+        plugins: [
+            resolve(),
+            commonjs(),
+            ts(),
+             json()
+            ],
         output: {
-            file: "dist/index.umd.js",
+             file: "dist/index.umd.js",
             format: "umd",
             name: "Index",
             plugins: [terser()],
